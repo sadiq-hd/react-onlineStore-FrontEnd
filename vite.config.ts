c1233365ee01fb186fed08470e691a6a0eaf5fca
sadiq <sadiqhd@gmail.com>
2024-12-08 02:35:09 +0300

    Update router configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-onlineStore-FrontEnd/home/',  // أضف هذا السطر
+  base: '/react-onlineStore-FrontEnd/',  // أضف هذا السطر
 
 })
