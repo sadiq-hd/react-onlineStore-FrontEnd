9c3feacadb9306dcd0d1f5e5bf33723fbf7ca362
sadiq <sadiqhd@gmail.com>
2024-12-08 00:22:27 +0300

    Update base URL for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
     
     <main className="container mx-auto px-4 py-8">
       <Routes>
-      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/Home" element={<Navigate to="/home" replace />} />
 
         <Route path="/home" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-onlineStore-FrontEnd/',  // أضف هذا السطر
+  base: '/',  // أضف هذا السطر
 
 })
